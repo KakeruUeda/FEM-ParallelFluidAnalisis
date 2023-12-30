aad01ad9146b840b2d862aa5bc74ce6c62c8d1b7
Kakeru Ueda <93113387+KakeruUeda@users.noreply.github.com>
2023-12-30 16:41:14 +0900

    delete src/lib/FEM

--- src/lib/FEM/basefem/DomainFEM.cpp
+++
@@ -1,2 +0,0 @@
-#include "domainFEM.h"
-
--- src/lib/FEM/pde/navierstokes/unsteady/XFEM_MatrixAssemblyUSNS.cpp
+++
@@ -1,7 +0,0 @@
-#include "FEM.h"
-
-void FEM::XFEM_MatAssyUSNS(MatrixXd &Klocal, VectorXd &Flocal, const int ic, const int t_itr)
-{
-  cout << "Not yet implemented" << endl; 
-  exit(1);
-}
