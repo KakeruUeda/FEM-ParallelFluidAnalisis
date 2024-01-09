8fd64cfb1fca9adb766f378d3a9786d4e4822c34
KakeruUeda <kakecoco2580@icloud.com>
2024-01-09 16:40:59 +0900

    update

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ rm -rf bin
 mkdir build
 cd build
 
-cmake -D CMAKE_INSTALL_PREFIX=/Users/kakeru/Documents/Projects/FluidDataAssimilation/bin \
+cmake -D CMAKE_INSTALL_PREFIX=/Users/kakeru/Documents/Projects/FEM-ParallelFluidAnalisis/bin \
       ..
 
 make && make install
